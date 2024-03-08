8fabd747b5e1779a939aff4c86aa39642b512fd0
Diogo Ferreira <diogo@underdev.org>
2024-03-08 10:44:42 +0000

    movement: <leader>ff switches to alternate file

--- init.lua
+++ init.lua
@@ -189,6 +189,9 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- File switching
+vim.keymap.set('n', '<leader>ff', '<C-^><CR>', { desc = '[F]ast switch to [F]ile in last used buffer' })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
